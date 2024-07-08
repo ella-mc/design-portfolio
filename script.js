a6654e5de703d650b5e6ff59c47a82eda70e084e
ella-mc <171029312+ella-mc@users.noreply.github.com>
2024-07-08 16:24:56 +0100

    Create file structure set up and top nav element

---
+++ script.js
@@ -0,0 +1,15 @@
+const navbar = document.querySelector('.navbar');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
